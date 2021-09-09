3009b690371078c2d82d92a6cc0cff76d0e42c8c
YPanda <k1868548@gmail.com>
2021-09-09 17:30:51 +0800

    feat(changelog): add log-format

--- bin/cli.js
+++ bin/cli.js
@@ -2,7 +2,7 @@
 
 /* istanbul ignore if */
 if (process.version.match(/v(\d+)\./)[1] < 6) {
-  console.error('standard-version: Node v6 or greater is required. `standard-version` did not run.')
+  console.error('pd-standard-version: Node v6 or greater is required. `pd-standard-version` did not run.')
 } else {
   const standardVersion = require('../index')
   const cmdParser = require('../command')
